7040a6da715df98a4b8d28c6537939e7161982e5
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-09 21:27:34 +0300

    Update Affair.tsx

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { AffairType } from './HW2';
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+   // key не нужно типизировать
+   affair: AffairType // need to fix any
+   deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+   const deleteCallback = (_id: number) => {
+      debugger
+      props.deleteAffairCallback(_id);
+   }// need to fix
 
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+   return (
+      <div>
+         {props.affair.name} {props.affair.priority}
+         <button onClick={() => deleteCallback(props.affair._id)}>X</button>
+      </div>
+   )
 }
 
 export default Affair
