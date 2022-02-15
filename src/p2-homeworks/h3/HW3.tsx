c076581bb5f1bfe5c51912a2ab38c95683c2b666
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-15 19:41:40 +0300

    Update HW3.tsx

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,34 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { v1 } from 'uuid'
 import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
-}
+   _id: string // need to fix any
+   name: string // need to fix any
+}[]
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+   const [users, setUsers] = useState<UserType>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
-    }
+   const addUserCallback = (name: string) => { // need to fix any
+      setUsers([...users, { _id: v1(), name: name }]) // need to fix
+   }
 
-    return (
-        <div>
-            <hr/>
-            homeworks 3
+   return (
+      <div>
+         <hr />
+         homeworks 3
 
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+         {/*should work (должно работать)*/}
+         <GreetingContainer users={users} addUserCallback={addUserCallback} />
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeGreeting/>*/}
-            <hr/>
-        </div>
-    )
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativeGreeting/>*/}
+         <hr />
+      </div>
+   )
 }
 
 export default HW3
