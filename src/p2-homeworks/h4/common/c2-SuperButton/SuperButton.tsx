9be0e5d2cf1a58792a3c6a2807b363e6496ddb1f
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-22 20:49:50 +0300

    Update SuperButton.tsx

--- src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
+++ src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
@@ -14,7 +14,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = `${red ? s.red : s.default} ${className}`
+    const finalClassName = `${s.default} ${red ? s.red : ''}`
 
     return (
         <button
