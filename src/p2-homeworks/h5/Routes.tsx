807e2a2855a55a7120bf9dc4131ae2e4c5dc1d27
Андрей Ашмянец <andreka6033@gmail.com>
2022-03-03 00:04:02 +0300

    Update Routes.tsx

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Error404 from './pages/Error404'
+import PreJunior from './pages/PreJunior'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    // add paths
+   PRE_JUNIOR: '/pre-junior',
+   // add paths
 }
 
 function Routes() {
-    return (
-        <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+   return (
+      <div>
+         {/*Switch выбирает первый подходящий роут*/}
+         <Routes>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR} />} />
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior />} />
                 // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404 />} />
 
-            {/*</Switch>*/}
-        </div>
-    )
+         </Routes>
+      </div>
+   )
 }
 
 export default Routes
