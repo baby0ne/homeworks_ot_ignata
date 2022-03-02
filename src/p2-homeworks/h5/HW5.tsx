9aa0647aa2bf343747c2d2612450e5c169b7bac2
Андрей Ашмянец <andreka6033@gmail.com>
2022-03-03 00:03:51 +0300

    Update HW5.tsx

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 import Header from './Header'
 import Routes from './Routes'
 
 function HW5() {
-    return (
-        <div>
-            {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+   return (
+      <div>
+         {/*в gh-pages лучше работает HashRouter*/}
+         <HashRouter>
 
-            <Header/>
+            <Header />
 
-            <Routes/>
+            <Routes />
 
-            {/*</HashRouter>*/}
-        </div>
-    )
+         </HashRouter>
+      </div>
+   )
 }
 
 export default HW5
