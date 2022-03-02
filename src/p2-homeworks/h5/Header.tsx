ff20eca73e876d04618016c69cd41c599b02a792
Андрей Ашмянец <andreka6033@gmail.com>
2022-03-03 00:47:35 +0300

    Update Header.tsx

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { PATH } from './Routes'
+import style from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-          <NavLink to={PATH.PRE_JUNIOR} >pre-junior</NavLink>
-        </div>
-    )
+   return (
+      <div className={style.navLinks}>
+         <NavLink to={PATH.PRE_JUNIOR} >pre-junior</NavLink>
+         <NavLink to={PATH.JUNIOR} >junior</NavLink>
+         <NavLink to={PATH.JUNIOR_PLUS} >junior-plus</NavLink>
+      </div>
+   )
 }
 
-export default Header
+export default Header;
