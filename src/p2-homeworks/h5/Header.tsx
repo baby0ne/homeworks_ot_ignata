226d1096a3089572e1b8a71bcbacaba7eae144c1
Андрей Ашмянец <andreka6033@gmail.com>
2022-03-03 00:03:48 +0300

    Update Header.tsx

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { PATH } from './Routes'
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+          <NavLink to={PATH.PRE_JUNIOR} >pre-junior</NavLink>
         </div>
     )
 }
