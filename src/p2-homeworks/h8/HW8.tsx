fb491870bbcb4abfea175054967289bbea2ee91b
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-01 00:14:24 +0300

    Update HW8.tsx

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,48 +1,56 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import React, { useState } from 'react';
+import { homeWorkReducer } from './bll/homeWorkReducer';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
+import style from './HW8.module.css';
 
-// export type UserType =
+export type UserType = {
+   _id: number
+   name: string
+   age: number
+};
 
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+const initialPeople: Array<UserType> = [
+   { _id: 0, name: 'Кот', age: 3 },
+   { _id: 1, name: 'Александр', age: 66 },
+   { _id: 2, name: 'Коля', age: 16 },
+   { _id: 3, name: 'Виктор', age: 44 },
+   { _id: 4, name: 'Дмитрий', age: 40 },
+   { _id: 5, name: 'Ирина', age: 55 },
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+   const [people, setPeople] = useState<Array<UserType>>(initialPeople)
 
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
+   const finalPeople = people.map((p: UserType) => (
+      <div key={p._id} className={style.container}>
+         <div>{p.name}</div>
+         <div>{p.age}</div>
+      </div>
+   ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+   const sortUp = () => setPeople(homeWorkReducer(initialPeople, { type: 'sort', payload: 'up' }));
+   const sortDown = () => setPeople(homeWorkReducer(initialPeople, { type: 'sort', payload: 'down' }));
+   const check = () => setPeople(homeWorkReducer(initialPeople, { type: 'check', payload: 18 }));
 
-    return (
-        <div>
-            <hr/>
-            homeworks 8
+   return (
+      <>
+         <hr />
+         homeworks 8
 
-            {/*should work (должно работать)*/}
-            {finalPeople}
+         {/*should work (должно работать)*/}
+         {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+         <SuperButton onClick={sortUp}>sort up</SuperButton>
+         <SuperButton onClick={sortDown}>sort down</SuperButton>
+         <SuperButton onClick={check}>check 18</SuperButton>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativePeople/>*/}
+         <hr />
+      </>
+   )
 }
 
 export default HW8
