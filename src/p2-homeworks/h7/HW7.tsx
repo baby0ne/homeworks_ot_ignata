799f28cd6a4757a796ca24f381179294ba555dff
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-01 00:13:30 +0300

    Update HW7.tsx

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 
-const arr = ['x', 'y', 'z']
+export type OptionsType = Array<string>;
+
+const arr = ['x', 'y', 'z'];
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+   const [value, onChangeOption] = useState<string>(arr[0])
 
     return (
         <div>
