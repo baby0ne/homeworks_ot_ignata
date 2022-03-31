f176f8ffd44b623c9ebbc963d88ba967eb18982c
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-01 00:13:35 +0300

    Update homeWorkReducer.ts

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,32 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
-    }
+import { UserType } from "../HW8"
+
+type SortUpAT = {
+   type: 'sort'
+   payload: 'up'
+}
+
+type SortDownAT = {
+   type: 'sort'
+   payload: 'down'
+}
+
+type CheckAT = {
+   type: 'check'
+   payload: 18
+}
+
+type ActionType = SortUpAT | SortDownAT | CheckAT;
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
+   switch (action.type) {
+      case 'sort': {
+         if (action.payload === 'up')
+            return [...state].sort((a, b) => a.name < b.name ? -1 : 1)
+         else return [...state].sort((a, b) => a.name > b.name ? -1 : 1)
+      }
+      case 'check': {
+         return state.filter(u => u.age >= action.payload)
+      }
+      default: return state
+   }
 }
