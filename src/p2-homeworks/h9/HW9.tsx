66a7e328e1622908cf859a1f2913bd4bb047e7ac
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-01 00:14:32 +0300

    Update HW9.tsx

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Clock from './Clock'
 
 function HW9() {
-    return (
-        <div>
-            <hr/>
-            homeworks 9
+   return (
+      <div>
+         <hr />
+         homeworks 9
 
-            {/*should work (должно работать)*/}
-            <Clock/>
+         {/*should work (должно работать)*/}
+         <Clock />
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
-            <hr/>
-        </div>
-    )
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativeClock/>*/}
+         <hr />
+      </div>
+   )
 }
 
 export default HW9
