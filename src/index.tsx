403c0dea4a66792c3d469c754857e0a5454c3f3e
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-08 16:53:00 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
+import store from './p2-homeworks/h10/bll/store'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById("root")
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById("root")
 )
 
 // If you want your app to work offline and load faster, you can change
