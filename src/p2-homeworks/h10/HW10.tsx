ce6bfe5917d8f24ae2d6e157784051c911bf5532
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-08 16:53:16 +0300

    Update HW10.tsx

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,38 +1,46 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import { loadingAC } from './bll/loadingReducer';
+import { AppStoreType } from './bll/store';
+import preloader from './preloader.svg';
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+   const dispatch = useDispatch();
+   const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading);
 
-    const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+   const setLoading = () => {
+      dispatch(loadingAC(true));
+      setTimeout(() => {
+         dispatch(loadingAC(false))
+      }, 2000)
+      console.log('loading...')
+   };
 
-    return (
-        <div>
-            <hr/>
-            homeworks 10
+   return (
+      <div>
+         <hr />
+         homeworks 10
 
-            {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
-                    <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
-                    </div>
-                )
-            }
+         {/*should work (должно работать)*/}
+         {loading
+            ? (
+               <div>
+                  <img src={preloader} />
+               </div>
+            ) : (
+               <div>
+                  <SuperButton onClick={setLoading}>set loading...</SuperButton>
+               </div>
+            )
+         }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
-        </div>
-    )
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<Alternative/>*/}
+         <hr />
+      </div>
+   )
 }
 
 export default HW10
