7ae5a347dc13583f5f361c7f8975218c108b7caf
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:52:27 +0300

    Update HW11.tsx

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+   const [value1, setValue1] = useState(0)
+   const [value2, setValue2] = useState(100)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+   const onChangeDoubleRange = (value: [number, number]) => {
+      if (value[1] <= value1) {
+         setValue2(value1);
+      } else {
+         setValue2(value[1]);
+      }
+      if (value[0] >= value2) {
+         setValue1(value2);
+      } else {
+         setValue1(value[0]);
+      }
+   }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+   const onChangeRange = (value: number) => {
+      if (value >= value2) {
+         setValue1(value2);
+      } else {
+         setValue1(value);
+      }
+   }
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+   return (
+      <div>
+         <hr />
+         homeworks 11
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+         {/*should work (должно работать)*/}
+         <div style={{ display: 'flex', justifyContent: 'space-between', width: '167px', marginBottom: '5px' }}>
+            <span>{value1}</span>
+            <SuperRange
+               value1={value1}
+               onChangeRange={onChangeRange}
+            />
+         </div>
+
+         <div style={{ display: 'flex', justifyContent: 'space-between', width: '190px' }}>
+            <span>{value1}</span>
+            <SuperDoubleRange
+               value={[value1, value2]}
+               onChangeRange={onChangeDoubleRange}
+            />
+            <span>{value2}</span>
+         </div>
+
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativeSuperRange/>*/}
+         {/*<AlternativeSuperDoubleRange/>*/}
+         <hr />
+      </div>
+   )
 }
 
 export default HW11
