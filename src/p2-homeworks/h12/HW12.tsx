25d2fb8a69cd48c6bccd1aa5a156cb29cccfb8e9
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:52:35 +0300

    Update HW12.tsx

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppStoreType } from "../h10/bll/store";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import { changeThemeAC } from "./bll/themesReducer";
 import s from "./HW12.module.css";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+   const theme = useSelector<AppStoreType, string>( state => state.themes.theme);
 
-    // useDispatch, onChangeCallback
+   const dispatch = useDispatch();
 
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
+   const onChangeCallback = (theme: string) => {
+      dispatch(changeThemeAC(theme));
+   }
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
+   return (
+      <div className={s[theme]}>
+         <hr />
+         <span className={s[theme + '-text']}>
+            homeworks 12
+         </span>
 
-            <hr/>
-        </div>
-    );
+         <div style={{ padding: '5px 0 0 15px' }}>
+            <SuperSelect
+               options={themes} 
+               value={theme}
+               onChangeOption={onChangeCallback}/>
+         </div>
+         <hr />
+      </div>
+   );
 }
 
 export default HW12;
