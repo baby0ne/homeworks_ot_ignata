87bb25187a4eabe32278d50fd15d25e4f7b17ac6
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:52:50 +0300

    Update Junior.tsx

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,6 +2,8 @@ import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
 import HW10 from "../../h10/HW10";
+import HW11 from "../../h11/HW11";
+import HW12 from "../../h12/HW12";
 
 function Junior() {
    return (
@@ -10,6 +12,8 @@ function Junior() {
          <HW8 />
          <HW9 />
          <HW10 />
+         <HW11 />
+         <HW12 />
       </div>
    )
 }
