693ccd12431aa5a27f661254b2f6adc027786a39
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:52:37 +0300

    Create HW13.tsx

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,18 @@
+import Request from "./Request";
+
+function HW13() {
+   return (
+      <div>
+         <hr />
+         <span >
+            homeworks 13
+         </span>
+         <div>
+            <Request />
+         </div>
+         <hr />
+      </div>
+   );
+}
+
+export default HW13;
