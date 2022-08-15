f81754a0212861772f580f9f8d45b7e7e0fe217c
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:53:04 +0300

    Update HW7.tsx

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 
@@ -9,35 +9,35 @@ const arr = ['x', 'y', 'z'];
 function HW7() {
    const [value, onChangeOption] = useState<string>(arr[0])
 
-    return (
-        <div>
-            <hr/>
-            homeworks 7
+   return (
+      <div>
+         <hr />
+         homeworks 7
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperSelect
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
-            <div>
-                <SuperRadio
-                    name={'radio'}
-                    options={arr}
-                    value={value}
-                    onChangeOption={onChangeOption}
-                />
-            </div>
+         {/*should work (должно работать)*/}
+         <div>
+            <SuperSelect
+               options={arr}
+               value={value}
+               onChangeOption={onChangeOption}
+            />
+         </div>
+         <div>
+            <SuperRadio
+               name={'radio'}
+               options={arr}
+               value={value}
+               onChangeOption={onChangeOption}
+            />
+         </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperSelect/>*/}
-            {/*<AlternativeSuperRadio/>*/}
-            <hr/>
-        </div>
-    )
+         <hr />
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativeSuperSelect/>*/}
+         {/*<AlternativeSuperRadio/>*/}
+         <hr />
+      </div>
+   )
 }
 
 export default HW7
