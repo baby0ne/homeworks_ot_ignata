c11b2c17a0992c7eba4205addfed306e958c5061
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 16:52:25 +0300

    Update SuperDoubleRange.tsx

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react';
+import style from './SuperDoubleRange.module.css';
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+   onChangeRange?: (value: [number, number]) => void
+   value: [number, number]
+   // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
+   {
+      onChangeRange, value,
+      // min, max, step, disable, ...
+   }
 ) => {
-    // сделать самому, можно подключать библиотеки
+   const onChangeCallbackFirst = (e: ChangeEvent<HTMLInputElement>) => {
+      onChangeRange && onChangeRange([+e.currentTarget.value, value[1]])
+   }
+   const onChangeCallbackSecond = (e: ChangeEvent<HTMLInputElement>) => {
+      onChangeRange && onChangeRange([value[0], +e.currentTarget.value])
+   }
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
+
+   return (
+      <div className={style.container}>
+         <input
+            type="range"
+            value={value[0]}
+            className={style.slider} 
+            onChange={onChangeCallbackFirst}/>
+         <input
+            type="range"
+            value={value[1]}
+            className={style.slider} 
+            onChange={onChangeCallbackSecond}/>
+      </div>
+   )
 }
 
 export default SuperDoubleRange
